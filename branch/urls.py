5fcaee8f1cbeaf0a0c69304c09cc8c6edac5b5f6
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-06-08 13:10:56 +0530

    required apps set up

--- branch/urls.py
+++ branch/urls.py
@@ -1,13 +1,12 @@
 # @Project:     FundConnect
 # @Filename:    urls.py
 # @Author:      Daksh
-# @Time:        08-06-2023 12:38 pm
-
+# @Time:        08-06-2023 01:09 pm
 from django.urls import path
 from . import views
 
-app_name = 'core'
+app_name = 'branch'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='branch-home'),
 ]
