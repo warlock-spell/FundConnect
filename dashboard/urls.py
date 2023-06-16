47d168875c2746a2ced4eea95242b1b4922f1c03
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-06-16 13:49:02 +0530

    add dashboard view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,5 +8,6 @@ from . import views
 app_name = 'dashboard'
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard-home'),
+    path('edit/', views.edit_dashboard, name='dashboard-edit'),
+    path('', views.view_dashboard, name='dashboard-home'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,18 +3,29 @@ from .forms import ControllerForm
 from .models import Controller
 
 
-def dashboard(request):
+def edit_dashboard(request):
     controller = Controller.objects.first()
 
     if request.method == 'POST':
         form = ControllerForm(request.POST, instance=controller)
         if form.is_valid():
+            print("Form is valid")
             form.save()
-            return redirect('core:home')
+            return redirect('dashboard:dashboard-home')
     else:
         form = ControllerForm(instance=controller)
 
     context = {
         'form': form,
     }
-    return render(request, 'dashboard/dashboard.html', context)
+    return render(request, 'dashboard/edit-dashboard.html', context)
+
+
+def view_dashboard(request):
+    controller = Controller.objects.first()  # Retrieve the first instance of Controller
+
+    context = {
+        'controller': controller
+    }
+
+    return render(request, 'dashboard/view-dashboard.html', context)
